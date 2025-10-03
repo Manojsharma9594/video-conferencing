3aac0434d77874823d9e689439a482b81977ff48
Manoj sharma <sharmamanoj1652005@gmail.com>
2025-10-03 13:13:42 +0530

    Project complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import LandingPage from "./pages/landing";
-import Authentication from "./pages/authentication";
-import { AuthProvider } from "./contexts/AuthContext";
-import VideoMeetComponent from "./pages/VideoMeet";
-// import HomeComponent from "./pages/home";
+import LandingPage from "./pages/landing.jsx";
+import Authentication from "./pages/authentication.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import VideoMeetComponent from "./pages/VideoMeet.jsx";
+import HomeComponent from "./pages/home";
 // import History from "./pages/history";
 
 function App() {
@@ -17,8 +17,8 @@ function App() {
 
             <Route path="/auth" element={<Authentication />} />
 
-            {/* <Route path="/home" s element={<HomeComponent />} />
-            <Route path="/history" element={<History />} /> */}
+            <Route path="/home" s element={<HomeComponent />} />
+            <Route path="/history" element={<History />} />
             <Route path="/:url" element={<VideoMeetComponent />} />
           </Routes>
         </AuthProvider>
